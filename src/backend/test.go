58bb0974f1123ec553dfc8f9e434a03cbd754680
ammarasyad <ammarasyad@gmail.com>
2023-05-04 15:34:58 +0700

    feat: Add test.go

---
+++ src/backend/test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	algorithm "src/backend/src/backend/algorithm"
+	sql_connection "src/backend/src/backend/sql"
+)
+
+func main() {
+	data_source := "root:password@/"
+	db, err := sql.Open("mysql", data_source)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	sql_connection.Create_Database(db)
+	println(algorithm.ProcessQuestion(db, "tambah pertanyaan papope dengan jawaban poppop", true))
+}
